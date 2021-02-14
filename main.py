15bab02eda04b9b4dd0630cad6484b48364723fd
rychlylukas <rychly-lukas@seznam.cz>
2021-02-14 20:32:26 +0100

    06

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-
-class MyGrid(Widget):
-    name = ObjectProperty(None)
-    email = ObjectProperty(None)
-
-    def btn(self):
-        print("Jméno:", self.name.text, "Email:", self.email.text)
-        self.name.text = ""
-        self.email.text = ""
-
+from kivy.uix.floatlayout import FloatLayout
 
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        return FloatLayout()
 
 if __name__ == "__main__":
     MyApp().run()
