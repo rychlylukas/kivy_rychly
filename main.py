02f6f7b0a78e1576fad4f31230eaf779828fbc0f
rychlylukas <rychly-lukas@seznam.cz>
2021-02-14 20:59:29 +0100

    08

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
+from kivy.graphics import Line
+
 
 class Touch(Widget):
-    btn = ObjectProperty(None)
+    def __init__(self, **kwargs):
+        super(Touch, self).__init__(**kwargs)
+
+        with self.canvas:
+            Color(1, 0, 1, 0.5, mode='rgba')
+            Line(points=(20, 30, 400, 500, 60, 500))
+            Color(1, 0, 0, 0.5, mode='rgba')
+            self.rect = Rectangle(pos=(0,0), size=(50,50))
+
 
     def on_touch_down(self, touch):
         print("Mouse Down", touch)
-        self.btn.opacity = 0.5
+        self.rect.pos = touch.pos
 
     def on_touch_move(self, touch):
+        self.rect.pos = touch.pos
         print("Mouse Move", touch)
-
-    def on_touch_up(self, touch):
-        print("Mouse UP", touch)
 
 
 class MyApp(App):
