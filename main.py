f451e422a378a0ba4575745913c2f06d05efc370
rychlylukas <rychly-lukas@seznam.cz>
2021-02-14 20:47:04 +0100

    07

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import kivy
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+class Touch(Widget):
+    btn = ObjectProperty(None)
+
+    def on_touch_down(self, touch):
+        print("Mouse Down", touch)
+        self.btn.opacity = 0.5
+
+    def on_touch_move(self, touch):
+        print("Mouse Move", touch)
+
+    def on_touch_up(self, touch):
+        print("Mouse UP", touch)
+
 
 class MyApp(App):
     def build(self):
-        return FloatLayout()
+        return Touch()
 
 if __name__ == "__main__":
     MyApp().run()
