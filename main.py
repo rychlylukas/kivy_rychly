6c47411a57ffea3e73ec6e2437da3dcda30c97be
rychlylukas <rychly-lukas@seznam.cz>
2021-02-14 21:23:47 +0100

    10

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import kivy
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
 
-class MainWindow(Screen):
+class Widgets(Widget):
+    def btn(self):
+        show_popup()
+
+class P(FloatLayout):
     pass
-
-class SecondWindow(Screen):
-    pass
-
-class WindowManager(ScreenManager):
-    pass
-
-kv = Builder.load_file("my.kv")
 
 class MyApp(App):
     def build(self):
-        return kv
+        return Widgets()
+
+def show_popup():
+    show = P()
+    popupWindow = Popup(title="Popup Windows", content=show, size_hint=(None, None), size=(400,400))
+    popupWindow.open()
+
 
 if __name__ == "__main__":
     MyApp().run()
